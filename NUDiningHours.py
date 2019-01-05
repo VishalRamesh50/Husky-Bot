43aa41decb6142f588f178ff5ba29c7ca7bde098
Vishal Ramesh <vishalramesh50@gmail.com>
2019-01-04 23:46:13 -0500

    Initial Commit.

---
+++ NUDiningHours.py
@@ -0,0 +1,33 @@
+IV = {'Monday': [7, 22, 'AM', 'PM'],
+      'Tuesday': [7, 22, 'AM,', 'PM'],
+      'Wednesday': [7, 22, 'AM', 'PM'],
+      'Thursday': [7, 22, 'AM', 'PM'],
+      'Friday': [7, 21, 'AM', 'PM'],
+      'Saturday': [8, 21, 'AM', 'PM'],
+      'Sunday': [8, 21, 'AM', 'PM']}
+STWEST = {'Monday': [11, 20, 'AM', 'PM'],
+          'Tuesday': [11, 20, 'AM', 'PM'],
+          'Wednesday': [11, 20, 'AM', 'PM'],
+          'Thursday': [11, 20, 'AM', 'PM'],
+          'Friday': [11, 17, 'AM', 'PM'],
+          'Saturday': "Closed",
+          'Sunday': [16, 20, 'PM', 'PM']}
+STEAST = {'Monday': [7, 23, 'AM', 'PM'],
+          'Tuesday': [7, 23, 'AM', 'PM'],
+          'Wednesday': [7, 23, 'AM', 'PM'],
+          'Thursday': [7, 23, 'AM', 'PM'],
+          'Friday': [7, 22, 'AM', 'PM'],
+          'Saturday': [8, 22, 'AM', 'PM'],
+          'Sunday': [8, 22, 'AM', 'PM']}
+OUTTAKES = {'Monday': [11, 13, 'AM', 'PM'],
+            'Tuesday': [11, 13, 'AM', 'PM'],
+            'Wednesday': [11, 13, 'AM', 'PM'],
+            'Thursday': [11, 13, 'AM', 'PM'],
+            'Friday': [11, 20, 'AM', 'PM'],
+            'Saturday': [12, 18, 'PM', 'PM'],
+            'Sunday': "Closed"}
+REBECCAS = {'Weekday': [7, 16, 'AM', 'PM'],
+            'Weekend': "Closed"}
+UBURGER = {'Weekday': [11, 20, 'AM', 'PM'],
+           'Saturday': [11, 19, 'AM', 'PM'],
+           'Sunday': [12, 19, 'PM', 'PM']}
