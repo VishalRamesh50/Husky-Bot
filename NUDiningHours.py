8976d1bad42b1b15531140562210ded3bb984569
Vishal Ramesh <vishalramesh50@gmail.com>
2019-01-06 23:44:01 -0500

    remove case-sensitive

--- NUDiningHours.py
+++ NUDiningHours.py
@@ -1,51 +1,51 @@
-IV = {'Monday': [7, '00', 'AM', 22, '00', 'PM'],
-      'Tuesday': [7, '00', 'AM', 22, '00', 'PM'],
-      'Wednesday': [7, '00', 'AM', 22, '00', 'PM'],
-      'Thursday': [7, '00', 'AM', 22, '00', 'PM'],
-      'Friday': [7, '00', 'AM', 21, '00', 'PM'],
-      'Saturday': [8, '00', 'AM', 21, '00', 'PM'],
-      'Sunday': [8, '00', 'AM', 21, '00', 'PM']}
-STWEST = {'Monday': [11, '00', 'AM', 20, '00', 'PM'],
-          'Tuesday': [11, '00', 'AM', 20, '00', 'PM'],
-          'Wednesday': [11, '00', 'AM', 20, '00', 'PM'],
-          'Thursday': [11, '00', 'AM', 20, '00', 'PM'],
-          'Friday': [11, '00', 'AM', 17, '00', 'PM'],
-          'Saturday': "Closed",
-          'Sunday': [16, '00', 'PM', 20, '00', 'PM']}
-STEAST = {'Monday': [7, '00', 'AM', 23, '00', 'PM'],
-          'Tuesday': [7, '00', 'AM', 23, '00', 'PM'],
-          'Wednesday': [7, '00', 'AM', 23, '00', 'PM'],
-          'Thursday': [7, '00', 'AM', 23, '00', 'PM'],
-          'Friday': [7, '00', 'AM', 22, '00', 'PM'],
-          'Saturday': [8, '00', 'AM', 22, '00', 'PM'],
-          'Sunday': [8, '00', 'AM', 22, '00', 'PM']}
-OUTTAKES = {'Monday': [11, '00', 'AM', 13, '00', 'PM'],
-            'Tuesday': [11, '00', 'AM', 13, '00', 'PM'],
-            'Wednesday': [11, '00', 'AM', 13, '00', 'PM'],
-            'Thursday': [11, '00', 'AM', 13, '00', 'PM'],
-            'Friday': [11, '00', 'AM', 20, '00', 'PM'],
-            'Saturday': [12, '00', 'PM', 18, '00', 'PM'],
-            'Sunday': "Closed"}
-REBECCAS = {'Weekday': [7, '00', 'AM', 16, '00', 'PM'],
-            'Weekend': "Closed"}
-UBURGER = {'Weekday': [11, '00', 'AM', 20, '00', 'PM'],
-           'Saturday': [11, '00', 'AM', 19, '00', 'PM'],
-           'Sunday': [12, '00', 'PM', 19, '00', 'PM']}
-KIGO = {'Monday': [11, '00', 'AM', 19, '00', 'PM'],
-        'Tuesday': [11, '00', 'AM', 19, '00', 'PM'],
-        'Wednesday': [11, '00', 'AM', 19, '00', 'PM'],
-        'Thursday': [11, '00', 'AM', 19, '00', 'PM'],
-        'Friday': [11, '00', 'AM', 16, '00', 'PM'],
-        'Weekend': "Closed"}
-STARBUCKS = {'Weekday': [7, '00', 'AM', 21, '00', 'PM'],
-             'Saturday': [8, '00', 'AM', 21, '00', 'PM'],
-             'Sunday': [10, '00', 'AM',  6, '00', 'PM']}
-SUBWAY = {'Monday': [8, '00', 'AM', 16, '30', 'PM'],
-          'Tuesday': [8, '00', 'AM', 16, '30', 'PM'],
-          'Wednesday': [8, '00', 'AM', 16, '30', 'PM'],
-          'Thursday': [8, '00', 'AM', 16, '30', 'PM'],
-          'Friday': [11, '00', 'AM', 16, '00', 'PM'],
-          'Weekend': "Closed"}
-POPEYES = {'Weekday': [10, '30', 'AM', 21, '00', 'PM'],
-           'Saturday': [10, '30', 'AM', 20, '00', 'PM'],
-           'Sunday': [12, '00', 'PM',  19, '00', 'PM']}
+IV = {'MONDAY': [7, '00', 'AM', 22, '00', 'PM'],
+      'TUESDAY': [7, '00', 'AM', 22, '00', 'PM'],
+      'WEDNESDAY': [7, '00', 'AM', 22, '00', 'PM'],
+      'THURSDAY': [7, '00', 'AM', 22, '00', 'PM'],
+      'FRIDAY': [7, '00', 'AM', 21, '00', 'PM'],
+      'SATURDAY': [8, '00', 'AM', 21, '00', 'PM'],
+      'SUNDAY': [8, '00', 'AM', 21, '00', 'PM']}
+STWEST = {'MONDAY': [11, '00', 'AM', 20, '00', 'PM'],
+          'TUESDAY': [11, '00', 'AM', 20, '00', 'PM'],
+          'WEDNESDAY': [11, '00', 'AM', 20, '00', 'PM'],
+          'THURSDAY': [11, '00', 'AM', 20, '00', 'PM'],
+          'FRIDAY': [11, '00', 'AM', 17, '00', 'PM'],
+          'SATURDAY': "Closed",
+          'SUNDAY': [16, '00', 'PM', 20, '00', 'PM']}
+STEAST = {'MONDAY': [7, '00', 'AM', 23, '00', 'PM'],
+          'TUESDAY': [7, '00', 'AM', 23, '00', 'PM'],
+          'WEDNESDAY': [7, '00', 'AM', 23, '00', 'PM'],
+          'THURSDAY': [7, '00', 'AM', 23, '00', 'PM'],
+          'FRIDAY': [7, '00', 'AM', 22, '00', 'PM'],
+          'SATURDAY': [8, '00', 'AM', 22, '00', 'PM'],
+          'SUNDAY': [8, '00', 'AM', 22, '00', 'PM']}
+OUTTAKES = {'MONDAY': [11, '00', 'AM', 13, '00', 'PM'],
+            'TUESDAY': [11, '00', 'AM', 13, '00', 'PM'],
+            'WEDNESDAY': [11, '00', 'AM', 13, '00', 'PM'],
+            'THURSDAY': [11, '00', 'AM', 13, '00', 'PM'],
+            'FRIDAY': [11, '00', 'AM', 20, '00', 'PM'],
+            'SATURDAY': [12, '00', 'PM', 18, '00', 'PM'],
+            'SUNDAY': "Closed"}
+REBECCAS = {'WEEKDAYS': [7, '00', 'AM', 16, '00', 'PM'],
+            'WEEKENDS': "Closed"}
+UBURGER = {'WEEKDAYS': [11, '00', 'AM', 20, '00', 'PM'],
+           'SATURDAY': [11, '00', 'AM', 19, '00', 'PM'],
+           'SUNDAY': [12, '00', 'PM', 19, '00', 'PM']}
+KIGO = {'MONDAY': [11, '00', 'AM', 19, '00', 'PM'],
+        'TUESDAY': [11, '00', 'AM', 19, '00', 'PM'],
+        'WEDNESDAY': [11, '00', 'AM', 19, '00', 'PM'],
+        'THURSDAY': [11, '00', 'AM', 19, '00', 'PM'],
+        'FRIDAY': [11, '00', 'AM', 16, '00', 'PM'],
+        'WEEKENDS': "Closed"}
+STARBUCKS = {'WEEKDAYS': [7, '00', 'AM', 21, '00', 'PM'],
+             'SATURDAY': [8, '00', 'AM', 21, '00', 'PM'],
+             'SUNDAY': [10, '00', 'AM',  6, '00', 'PM']}
+SUBWAY = {'MONDAY': [8, '00', 'AM', 16, '30', 'PM'],
+          'TUESDAY': [8, '00', 'AM', 16, '30', 'PM'],
+          'WEDNESDAY': [8, '00', 'AM', 16, '30', 'PM'],
+          'THURSDAY': [8, '00', 'AM', 16, '30', 'PM'],
+          'FRIDAY': [11, '00', 'AM', 16, '00', 'PM'],
+          'WEEKENDS': "Closed"}
+POPEYES = {'WEEKDAYS': [10, '30', 'AM', 21, '00', 'PM'],
+           'SATURDAY': [10, '30', 'AM', 20, '00', 'PM'],
+           'SUNDAY': [12, '00', 'PM',  19, '00', 'PM']}
