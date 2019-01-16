2d3aeaac29b7924bd82e3cd20b84ce4cdbd4a3d8
Vishal Ramesh <vishalramesh50@gmail.com>
2019-01-16 13:41:22 -0500

    rename file, add ice cream flavors steast/stwest

---
+++ NUDining.py
@@ -0,0 +1,62 @@
+IV = {'MONDAY': [7, '00', 'AM', 22, '00', 'PM'],
+      'TUESDAY': [7, '00', 'AM', 22, '00', 'PM'],
+      'WEDNESDAY': [7, '00', 'AM', 22, '00', 'PM'],
+      'THURSDAY': [7, '00', 'AM', 22, '00', 'PM'],
+      'FRIDAY': [7, '00', 'AM', 21, '00', 'PM'],
+      'WEEKENDS': [8, '00', 'AM', 21, '00', 'PM']}
+STWEST = {'MONDAY': [11, '00', 'AM', 20, '00', 'PM'],
+          'TUESDAY': [11, '00', 'AM', 20, '00', 'PM'],
+          'WEDNESDAY': [11, '00', 'AM', 20, '00', 'PM'],
+          'THURSDAY': [11, '00', 'AM', 20, '00', 'PM'],
+          'FRIDAY': [11, '00', 'AM', 17, '00', 'PM'],
+          'SATURDAY': "Closed",
+          'SUNDAY': [16, '00', 'PM', 20, '00', 'PM']}
+STEAST = {'MONDAY': [7, '00', 'AM', 23, '00', 'PM'],
+          'TUESDAY': [7, '00', 'AM', 23, '00', 'PM'],
+          'WEDNESDAY': [7, '00', 'AM', 23, '00', 'PM'],
+          'THURSDAY': [7, '00', 'AM', 23, '00', 'PM'],
+          'FRIDAY': [7, '00', 'AM', 22, '00', 'PM'],
+          'WEEKEENDS': [8, '00', 'AM', 22, '00', 'PM']}
+OUTTAKES = {'MONDAY': [11, '00', 'AM', 13, '00', 'PM'],
+            'TUESDAY': [11, '00', 'AM', 13, '00', 'PM'],
+            'WEDNESDAY': [11, '00', 'AM', 13, '00', 'PM'],
+            'THURSDAY': [11, '00', 'AM', 13, '00', 'PM'],
+            'FRIDAY': [11, '00', 'AM', 20, '00', 'PM'],
+            'SATURDAY': [12, '00', 'PM', 18, '00', 'PM'],
+            'SUNDAY': "Closed"}
+REBECCAS = {'WEEKDAYS': [7, '00', 'AM', 16, '00', 'PM'],
+            'WEEKENDS': "Closed"}
+UBURGER = {'WEEKDAYS': [11, '00', 'AM', 20, '00', 'PM'],
+           'SATURDAY': [11, '00', 'AM', 19, '00', 'PM'],
+           'SUNDAY': [12, '00', 'PM', 19, '00', 'PM']}
+KIGO = {'MONDAY': [11, '00', 'AM', 19, '00', 'PM'],
+        'TUESDAY': [11, '00', 'AM', 19, '00', 'PM'],
+        'WEDNESDAY': [11, '00', 'AM', 19, '00', 'PM'],
+        'THURSDAY': [11, '00', 'AM', 19, '00', 'PM'],
+        'FRIDAY': [11, '00', 'AM', 16, '00', 'PM'],
+        'WEEKENDS': "Closed"}
+STARBUCKS = {'WEEKDAYS': [7, '00', 'AM', 21, '00', 'PM'],
+             'SATURDAY': [8, '00', 'AM', 21, '00', 'PM'],
+             'SUNDAY': [10, '00', 'AM',  6, '00', 'PM']}
+SUBWAY = {'MONDAY': [8, '00', 'AM', 16, '30', 'PM'],
+          'TUESDAY': [8, '00', 'AM', 16, '30', 'PM'],
+          'WEDNESDAY': [8, '00', 'AM', 16, '30', 'PM'],
+          'THURSDAY': [8, '00', 'AM', 16, '30', 'PM'],
+          'FRIDAY': [11, '00', 'AM', 16, '00', 'PM'],
+          'WEEKENDS': "Closed"}
+POPEYES = {'WEEKDAYS': [10, '30', 'AM', 21, '00', 'PM'],
+           'SATURDAY': [10, '30', 'AM', 20, '00', 'PM'],
+           'SUNDAY': [12, '00', 'PM',  19, '00', 'PM']}
+QDOBA = {'MONDAY': [10, '30', 'AM', 23, '00', 'PM'],
+         'TUESDAY': [10, '30', 'AM', 23, '00', 'PM'],
+         'WEDNESDAY': [10, '30', 'AM', 23, '00', 'PM'],
+         'THURSDAY': [10, '30', 'AM', 24, '00', 'AM'],
+         'FRIDAY': [10, '30', 'AM', 24, '00', 'AM'],
+         'WEEKEENDS': [10, '30', 'AM', 20, '00', 'PM']}
+FLAVORS_STEAST_STWEST = {'MONDAY': 'STRAWBERRY & CHOCOLATE 🍨',
+                         'TUESDAY': 'RASPBERRY CHOCOLATE & VANILLA 🍨',
+                         'WEDNESDAY': 'VANILLA & MINT CHOCOLATE 🍨',
+                         'THURSDAY': 'ORANGE CREAM & VANILLA 🍨',
+                         'FRIDAY': 'MOCHA & VANILLA 🍨',
+                         'SATURDAY': 'CHOCOLATE & COCONUT PINEAPPLE 🍨',
+                         'SUNDAY': 'VANILLA & MAPLE 🍨'}
