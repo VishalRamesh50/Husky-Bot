5fce325409487333a89e80bed11563d876dc8465
Vishal Ramesh <vishalramesh50@gmail.com>
2019-01-18 19:53:29 -0500

    Reordered dictionaries, added MLK Weekend support

--- NUDining.py
+++ NUDining.py
@@ -1,9 +1,16 @@
+# Normal Hours
 IV = {'MONDAY': [7, '00', 'AM', 22, '00', 'PM'],
       'TUESDAY': [7, '00', 'AM', 22, '00', 'PM'],
       'WEDNESDAY': [7, '00', 'AM', 22, '00', 'PM'],
       'THURSDAY': [7, '00', 'AM', 22, '00', 'PM'],
       'FRIDAY': [7, '00', 'AM', 21, '00', 'PM'],
       'WEEKENDS': [8, '00', 'AM', 21, '00', 'PM']}
+STEAST = {'MONDAY': [7, '00', 'AM', 23, '00', 'PM'],
+          'TUESDAY': [7, '00', 'AM', 23, '00', 'PM'],
+          'WEDNESDAY': [7, '00', 'AM', 23, '00', 'PM'],
+          'THURSDAY': [7, '00', 'AM', 23, '00', 'PM'],
+          'FRIDAY': [7, '00', 'AM', 22, '00', 'PM'],
+          'WEEKEENDS': [8, '00', 'AM', 22, '00', 'PM']}
 STWEST = {'MONDAY': [11, '00', 'AM', 20, '00', 'PM'],
           'TUESDAY': [11, '00', 'AM', 20, '00', 'PM'],
           'WEDNESDAY': [11, '00', 'AM', 20, '00', 'PM'],
@@ -11,30 +18,24 @@ STWEST = {'MONDAY': [11, '00', 'AM', 20, '00', 'PM'],
           'FRIDAY': [11, '00', 'AM', 17, '00', 'PM'],
           'SATURDAY': "Closed",
           'SUNDAY': [16, '00', 'PM', 20, '00', 'PM']}
-STEAST = {'MONDAY': [7, '00', 'AM', 23, '00', 'PM'],
-          'TUESDAY': [7, '00', 'AM', 23, '00', 'PM'],
-          'WEDNESDAY': [7, '00', 'AM', 23, '00', 'PM'],
-          'THURSDAY': [7, '00', 'AM', 23, '00', 'PM'],
-          'FRIDAY': [7, '00', 'AM', 22, '00', 'PM'],
-          'WEEKEENDS': [8, '00', 'AM', 22, '00', 'PM']}
 OUTTAKES = {'MONDAY': [11, '00', 'AM', 13, '00', 'PM'],
             'TUESDAY': [11, '00', 'AM', 13, '00', 'PM'],
             'WEDNESDAY': [11, '00', 'AM', 13, '00', 'PM'],
-            'THURSDAY': [11, '00', 'AM', 13, '00', 'PM'],
+            'THURSDAY': [11, '00', 'AM', 25, '00', 'AM'],
             'FRIDAY': [11, '00', 'AM', 20, '00', 'PM'],
             'SATURDAY': [12, '00', 'PM', 18, '00', 'PM'],
             'SUNDAY': "Closed"}
-REBECCAS = {'WEEKDAYS': [7, '00', 'AM', 16, '00', 'PM'],
-            'WEEKENDS': "Closed"}
-UBURGER = {'WEEKDAYS': [11, '00', 'AM', 20, '00', 'PM'],
-           'SATURDAY': [11, '00', 'AM', 19, '00', 'PM'],
-           'SUNDAY': [12, '00', 'PM', 19, '00', 'PM']}
 KIGO = {'MONDAY': [11, '00', 'AM', 19, '00', 'PM'],
         'TUESDAY': [11, '00', 'AM', 19, '00', 'PM'],
         'WEDNESDAY': [11, '00', 'AM', 19, '00', 'PM'],
         'THURSDAY': [11, '00', 'AM', 19, '00', 'PM'],
         'FRIDAY': [11, '00', 'AM', 16, '00', 'PM'],
         'WEEKENDS': "Closed"}
+POPEYES = {'WEEKDAYS': [10, '30', 'AM', 21, '00', 'PM'],
+           'SATURDAY': [10, '30', 'AM', 20, '00', 'PM'],
+           'SUNDAY': [12, '00', 'PM',  19, '00', 'PM']}
+REBECCAS = {'WEEKDAYS': [7, '00', 'AM', 16, '00', 'PM'],
+            'WEEKENDS': "Closed"}
 STARBUCKS = {'WEEKDAYS': [7, '00', 'AM', 21, '00', 'PM'],
              'SATURDAY': [8, '00', 'AM', 21, '00', 'PM'],
              'SUNDAY': [10, '00', 'AM',  6, '00', 'PM']}
@@ -44,9 +45,9 @@ SUBWAY = {'MONDAY': [8, '00', 'AM', 16, '30', 'PM'],
           'THURSDAY': [8, '00', 'AM', 16, '30', 'PM'],
           'FRIDAY': [11, '00', 'AM', 16, '00', 'PM'],
           'WEEKENDS': "Closed"}
-POPEYES = {'WEEKDAYS': [10, '30', 'AM', 21, '00', 'PM'],
-           'SATURDAY': [10, '30', 'AM', 20, '00', 'PM'],
-           'SUNDAY': [12, '00', 'PM',  19, '00', 'PM']}
+UBURGER = {'WEEKDAYS': [11, '00', 'AM', 20, '00', 'PM'],
+           'SATURDAY': [11, '00', 'AM', 19, '00', 'PM'],
+           'SUNDAY': [12, '00', 'PM', 19, '00', 'PM']}
 QDOBA = {'MONDAY': [10, '30', 'AM', 23, '00', 'PM'],
          'TUESDAY': [10, '30', 'AM', 23, '00', 'PM'],
          'WEDNESDAY': [10, '30', 'AM', 23, '00', 'PM'],
@@ -60,3 +61,46 @@ ICE_CREAM_FLAVORS = {'MONDAY': 'STRAWBERRY & CHOCOLATE 🍨',
                      'FRIDAY': 'MOCHA & VANILLA 🍨',
                      'SATURDAY': 'CHOCOLATE & COCONUT PINEAPPLE 🍨',
                      'SUNDAY': 'VANILLA & MAPLE 🍨'}
+
+# Martin Luther King Weekend hours
+IV_MARTIN = {'FRIDAY': [7, '00', 'AM', 21, '00', 'PM'],
+             'SATURDAY': [8, '00', 'AM', 20, '00', 'PM'],
+             'SUNDAY': [8, '00', 'AM', 20, '00', 'PM'],
+             'MONDAY': [8, '00', 'AM', 22, '00', 'PM']}
+STEAST_MARTIN = {'FRIDAY': [7, '00', 'AM', 21, '00', 'PM'],
+                 'SATURDAY': [8, '00', 'AM', 20, '00', 'PM'],
+                 'SUNDAY': [8, '00', 'AM', 20, '00', 'PM'],
+                 'MONDAY': [8, '00', 'AM', 23, '00', 'PM']}
+STWEST_MARTIN = {'FRIDAY': [11, '00', 'AM', 15, '00', 'PM'],
+                 'SATURDAY': 'Closed',
+                 'SUNDAY': 'Closed',
+                 'MONDAY': [16, '00', 'AM', 20, '00', 'PM']}
+OUTTAKES_MARTIN = {'FRIDAY': [11, '00', 'AM', 19, '00', 'PM'],
+                   'SATURDAY': 'Closed',
+                   'SUNDAY': 'Closed',
+                   'MONDAY': 'Closed'}
+KIGO_MARTIN = {'FRIDAY': [11, '00', 'AM', 15, '00', 'PM'],
+               'SATURDAY': 'Closed',
+               'SUNDAY': 'Closed',
+               'MONDAY': 'Closed'}
+POPEYES_MARTIN = {'FRIDAY': [10, '30', 'AM', 20, '00', 'PM'],
+                  'SATURDAY': [11, '30', 'AM', 19, '00', 'PM'],
+                  'SUNDAY': [12, '00', 'PM', 18, '00', 'PM'],
+                  'MONDAY': [11, '30', 'AM', 20, '00', 'PM']}
+REBECCAS_MARTIN = {'FRIDAY': [7, '00', 'AM', 16, '00', 'PM'],
+                   'SATURDAY': 'Closed',
+                   'SUNDAY': 'Closed',
+                   'MONDAY': [7, '00', 'AM', 14, '00', 'PM']}
+STARBUCKS_MARTIN = {'FRIDAY': [7, '00', 'AM', 21, '00', 'PM'],
+                    'SATURDAY': [10, '30', 'AM', 21, '00', 'PM'],
+                    'SUNDAY': 'Closed',
+                    'MONDAY': [10, '30', 'AM', 9, '00', 'PM']}
+SUBWAY_MARTIN = {'FRIDAY': [8, '00', 'AM', 16, '00', 'PM'],
+                 'SATURDAY': 'Closed',
+                 'SUNDAY': 'Closed',
+                 'MONDAY': 'Closed'}
+UBURGER_MARTIN = {'FRIDAY': [11, '00', 'AM', 20, '00', 'PM'],
+                  'SATURDAY': 'Closed',
+                  'SUNDAY': 'Closed',
+                  'MONDAY': [11, '30', 'AM', 19, '00', 'PM']}
+QDOBA_MARTIN = QDOBA
