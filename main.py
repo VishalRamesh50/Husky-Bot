83eacd00bd6a339a28141dcd926d777db8d5cb40
Vishal Ramesh <vishalramesh50@gmail.com>
2019-01-19 20:49:24 -0500

    improved commenting, auto-handle Not Registered role adding/removing, add opening_soon message

--- main.py
+++ main.py
@@ -52,14 +52,16 @@ async def change_status():
         await asyncio.sleep(5)  # change status every 5 seconds
 
 
-@client.event  # error handler
+# error handler
+@client.event
 async def on_command_error(error, ctx):
     channel = ctx.message.channel
     if isinstance(error, commands.CheckFailure):  # if command was not used with required role
         await client.send_message(channel, 'Insufficient Role')
 
 
-@client.event  # Welcome Message
+# Welcome Message
+@client.event
 async def on_member_join(member):
     embed = discord.Embed(
         description=f"Hey {member.mention}, welcome to **NU** 🎉! Check your DMs from <@!{DYNO_BOT_ID}> for further instructions!",
@@ -68,7 +70,8 @@ async def on_member_join(member):
     await client.send_message(client.get_channel(GENERAL_CHANNEL_ID), embed=embed)
 
 
-@client.event  # AutoDelete Dyno Bot's Messages in #course-registration
+# AutoDelete Dyno Bot's Messages in #course-registration
+@client.event
 async def on_message(message):
     if message.author.id == DYNO_BOT_ID and message.channel.id == COURSE_REGISTRATION_CHANNEL_ID:
         await asyncio.sleep(5)
@@ -97,7 +100,36 @@ async def on_message(message):
     await client.process_commands(message)
 
 
-@client.command(pass_context=True)  # help command
+# Removes Not Registered Role from fully registered members or adds it to unregistered ones
+@client.event
+async def on_member_update(before, after):
+    POSSIBLE_YEARS = ['Freshman', 'Sophomore', 'Middler', 'Junior', 'Senior', 'Graduate']
+    POSSIBLE_SCHOOLS = ['EXPLORE', 'COE', 'CCIS', 'CAMD', 'DMSB', 'BCHS', 'CPS', 'CSSH', 'COS']
+    STUDENT = ['Student']
+    # get Not Registered Role Object
+    NOT_REGISTERED_ROLE = discord.utils.get(after.server.roles, name="Not Registered")
+    in_years, in_schools, in_student = False, False, False
+    # if roles have changed
+    if before.roles != after.roles:
+        # iterate through members roles
+        for role in after.roles:
+            # if member's role is a Year
+            if role.name in POSSIBLE_YEARS:
+                in_years = True
+            # if member's role is a School
+            elif role.name in POSSIBLE_SCHOOLS:
+                in_schools = True
+            # if member's role is Student
+            elif role.name in STUDENT:
+                in_student = True
+        if in_years and in_schools and in_student:
+            await client.remove_roles(after, NOT_REGISTERED_ROLE)
+        else:
+            await client.add_roles(after, NOT_REGISTERED_ROLE)
+
+
+# help command
+@client.command(pass_context=True)
 async def help(ctx):
     author = ctx.message.author
 
@@ -133,23 +165,26 @@ async def help(ctx):
     await client.send_message(author, embed=embed)  # sends user a DM
 
 
-@client.command()  # replies Pong! given .ping
+# replies Pong! given .ping
+@client.command()
 async def ping():
     await client.say('Pong!')
 
 
-@client.command(pass_context=True)  # repeats given statement after .echo
+# repeats given statement after .echo
+@client.command(pass_context=True)
 async def echo(ctx, *args):
     message = ctx.message
     output = ''
     for word in args:
         output += word
         output += ' '
-    await client.delete_message(message)
+    await client.delete_message(message)  # deletes command
     await client.say(output)
 
 
-@client.command(pass_context=True)  # deletes set amount of messages
+# deletes set amount of messages
+@client.command(pass_context=True)
 @commands.has_role('Admin')
 async def clear(ctx, amount=''):
     channel = ctx.message.channel
@@ -165,6 +200,7 @@ async def clear(ctx, amount=''):
         await client.say("Error: Must use a number.")
 
 
+# Husky Bot self-introduction
 @client.command(pass_context=True)
 @commands.has_role('Admin')
 async def introduction(ctx):
@@ -226,9 +262,10 @@ async def on_message_edit(message1, message2):
 async def not_registered_reminder():  # sends announcement to register 2 times a week
     await client.wait_until_ready()
     reminder = (f":pushpin: <@&{NOT_REGISTERED_ID}> you either haven't registered to become a Student, chosen your year, or declared a major yet!\n"
-                f":one: Head over to {client.get_channel(RULES_CHANNEL_ID).mention} , read and accept by reacting with a :thumbsup: and choose your year.\n"
-                f":two: Next, head over to {client.get_channel(COURSE_REGISTRATION_CHANNEL_ID).mention} and choose your major and courses now!\n"
-                f":three: Finally, type `?choose Not Registered` in {client.get_channel(NOT_REGISTERED_CHANNEL_ID).mention} . You now are an official member!")
+                f":one: Accept the {client.get_channel(RULES_CHANNEL_ID).mention} by reacting with a :thumbsup:\n"
+                f":two: Select your year by reacting with a number."
+                f":three: Assign yourself a school/major and courses in {client.get_channel(COURSE_REGISTRATION_CHANNEL_ID).mention}\n"
+                f"You now are an official member!")
 
     while not client.is_closed:
         await client.send_message(client.get_channel(TEST_CHANNEL_ID), reminder)
@@ -236,13 +273,14 @@ async def not_registered_reminder():  # sends announcement to register 2 times a
 '''
 
 
-@client.command(pass_context=True)  # reminder
+# Reminds the user of anything in a set duration of time
+@client.command(pass_context=True)
 async def reminder(ctx, *args):
     author = ctx.message.author
     reminder = ''
-    original_time = args[len(args)-2]
+    original_time = args[len(args)-2]  # user's specified time frame
     try:
-        time = Decimal(original_time)  # second to last term -> time as a string
+        time = Decimal(original_time)  # converts user's time (String) to a Decimal
     except decimal.InvalidOperation:  # if time is not a number
         await client.say(f"You have to use a number for the 2nd to last term.\n"
                          f"Incorrect: `.reminder Husky Bot is cool in five secs`\n"
@@ -254,6 +292,7 @@ async def reminder(ctx, *args):
     DAY_POSSIBILITIES = ('day', 'days', 'd')
     WEEK_POSSIBILITIES = ('week', 'weeks', 'w')
     valid_unit = True
+    # converts time to seconds
     if UNIT_OF_TIME in SECOND_POSSIBILITIES:
         time = time
     elif UNIT_OF_TIME in MINUTE_POSSIBILITIES:
@@ -268,11 +307,14 @@ async def reminder(ctx, *args):
         valid_unit = False
     time = int(time)
     for index in range(len(args)):
+        # once then end of the reminder has been reached break
         if index == len(args)-3 and args[index] == 'in':
             break
         else:
+            # adds word to reminder
             reminder += args[index]
             reminder += ' '
+    # if "in" is not between the reminder and the time
     if "in" != args[len(args)-3]:
         await client.say(f"You must include the word `in` between your reminder and the time.\n"
                          f"Incorrect: `.reminder Husky Bot is cool 5 secs`\n"
@@ -280,7 +322,7 @@ async def reminder(ctx, *args):
     else:
         if valid_unit:
             await client.say(f"I will remind you about `{reminder}` in `{original_time} {UNIT_OF_TIME}`")
-            await asyncio.sleep(time)
+            await asyncio.sleep(time)  # wait for specified time in seconds before sending reminder
             await client.send_message(author, f"Here is your reminder for `{reminder}`")
         else:  # if not a valid unit of time
             await client.say(f"Your unit of measurement must be a second, minute, hour, day, or week.\n"
@@ -288,49 +330,59 @@ async def reminder(ctx, *args):
                              f"Correct: `.reminder Husky Bot is cool in 4 weeks`")
 
 
-@client.command()  # generates invite link to server
+# generates invite link to server
+@client.command()
 async def invite():
     await client.say('discord.gg/CP9MBRH')
 
 
-@client.command()  # generates link to NU dining menu
+# generates link to NU dining menu
+@client.command()
 async def menu():
     await client.say('https://new.dineoncampus.com/Northeastern/menus')
 
 
-@client.command()  # tells what day a date is
+# tells what day a date is
+@client.command()
 async def day(*args):
     EST = datetime.now(timezone('US/Eastern'))
     POSSIBLE_MONTHS_FULL = ("JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER")
     POSSIBLE_MONTHS_SHORT = ("JAN", "FEB", "MAR", "APR", "MAY", "JUNE", "JULY", "AUG", "SEPT", "OCT", "NOV", "DEC")
+    # if date is in MM/DD/(YYYY) format
     if len(args) == 1 and '/' in args[0]:
         args = args[0].split('/')
         month = args[0]
         try:
-            if 0 < int(month) <= 12:
+            if 0 < int(month) <= 12:  # valid month
                 month = int(month)
-            else:
+            else:  # invalid month
                 await client.say('Month must be 1-12')
-        except ValueError:
+        except ValueError:  # month not a number
             await client.say("Month is must be a number.")
-    else:
+    else:  # date in Month Day (Year) format
         month = args[0]
+        # if month is not in full form or acryonymed
         if month.upper() not in POSSIBLE_MONTHS_FULL and month.upper() not in POSSIBLE_MONTHS_SHORT:
             await client.say("Please try a valid month.")
         else:
+            # convert given month in words to associated numerical month
             for index in range(0, 12):
                 if month.upper() == POSSIBLE_MONTHS_FULL[index] or month.upper() == POSSIBLE_MONTHS_SHORT[index]:
                     month = index + 1
                     break
+    # if year is given
     if len(args) > 2:
         try:
             year = int(args[2])
+        # if year not a number
         except ValueError:
             await client.say("Year must be a number.")
     else:
+        # use current year
         year = int(EST.strftime("%Y"))
     try:
         date = int(args[1])
+    # if date not a number
     except ValueError:
         await client.say("Date needs to be a number.")
     try:
@@ -344,30 +396,42 @@ async def day(*args):
     await client.say(f"{month}/{date}/{year} is a {day}")
 
 
-@client.command()  # gives the hours of operation for select locations and determines whether open or not
+# gives the hours of operation for select locations and determines whether open or not
+@client.command()
 async def hours(*args):
     content = args[0].upper()
     POSSIBLE_DAYS = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY']
+    POSSIBLE_LOCATIONS = "IV, Steast, Stwest, Outtakes, Kigo's Kitchen, Popeyes, Rebecca's, Starbucks, Subway, UBurger, Qdoba."
     EST = datetime.now(timezone('US/Eastern'))
+    # if location and day is given
     if len(args) >= 2:
+        # if given day is tomorrow
         if args[1].upper() == 'TOMORROW':
+            # finds the current day's index in POSSIBLE_DAYS and add 1 getting the next day
             for index in range(0, len(POSSIBLE_DAYS)):
                 if EST.strftime("%A").upper() == POSSIBLE_DAYS[index]:
                     day = POSSIBLE_DAYS[(index+1) % len(POSSIBLE_DAYS)]
                     break
+        # if given day is a valid day
         elif args[1].upper() in POSSIBLE_DAYS:
             day = args[1].upper()
+        # if given day is invalid
         else:
             await client.say("Error: Not a valid day.")
+    # if no day is given
     else:
+        # set day to be current day
         day = EST.strftime("%A").upper()
     hour = int(EST.strftime("%H"))
     minute = int(EST.strftime("%M"))
-    valid_location = True
+    valid_location = True  # whether the given location is valid or not
     month = int(EST.strftime("%m"))
     date = int(EST.strftime("%d"))
     year = int(EST.strftime("%Y"))
-    if month == 1 and 18 <= date <= 21 and year == 2019:  # Martin Luther King Weeekend (2019)
+    normal = True  # whether it's normal hours or not
+    # Martin Luther King Weeekend (2019)
+    if month == 1 and 18 <= date <= 21 and year == 2019:
+        # if specified day is during MLK Weekend
         if day in ['FRIDAY', 'SATURDAY', 'SUNDAY', 'MONDAY']:
             normal = False
             holiday = " **(Martin Luther King Weekend)**"
@@ -395,10 +459,9 @@ async def hours(*args):
                 location = NUDining.QDOBA_MARTIN
             else:
                 valid_location = False
-        else:
-            normal = True
+    # if normal hours
     if normal:
-        holiday = ''
+        holiday = ''  # no holiday
         if content == "IV":
             if day[0] == 'S':
                 day = 'WEEKENDS'
@@ -441,26 +504,33 @@ async def hours(*args):
             location = NUDining.QDOBA
         else:
             valid_location = False
+    # if given location is a valid location
     if valid_location:
-        if location[day] == "Closed":
+        # if location is closed for the whole day
+        if location[day] == "CLOSED":
             await client.say(f"{content} is CLOSED {day}{holiday}.")
-            await client.say('https://nudining.com/hours')
         else:
-            opening = location[day][0]
-            opening_hour = opening % 12
+            opening = location[day][0]  # opening hour in 24hr format
+            opening_hour = opening % 12  # opening hour in 12hr format
+            # since 12%12=0, convert to 12
             if opening_hour == 0:
                 opening_hour = 12
             opening_minute = location[day][1]
-            opening_period = location[day][2]
-            closing = location[day][3]
-            closing_hour = closing % 12
+            opening_period = location[day][2]  # AM/PM
+            closing = location[day][3]  # closing hour in 24hr format
+            closing_hour = closing % 12  # closing hour in 12hr format
+            # since 12%12=0, convert to 12
             if closing_hour == 0:
                 closing_hour = 12
             closing_minute = location[day][4]
-            closing_period = location[day][5]
-            hours_of_operation = f"It's open from {opening_hour}:{opening_minute} {opening_period} - {closing_hour}:{closing_minute} {closing_period} on {day}{holiday}."
+            closing_period = location[day][5]  # AM/PM
+            hours_of_operation = f"{content} is open from {opening_hour}:{opening_minute} {opening_period} - {closing_hour}:{closing_minute} {closing_period} on {day}{holiday}."
             open = client.say(f"{content} is OPEN now! {hours_of_operation}")
             closed = client.say(f"{content} is CLOSED now. {hours_of_operation}")
+            difference = 0
+            opening_soon = client.say(f"{content} is CLOSED now. {hours_of_operation} It will be opening in {difference} minutes!")
+            closing_soon = client.say(f"{content} is OPEN now! {hours_of_operation} It will be closing in {difference} minutes!")
+            # if a day was specified
             if len(args) >= 2:
                 await client.say(hours_of_operation)
             else:
@@ -469,49 +539,69 @@ async def hours(*args):
                         if minute >= int(opening_minute):
                             await open
                         else:
-                            await closed
+                            difference = int(opening_minute) - minute
+                            await opening_soon
                     elif hour == closing:
                         if minute < int(closing_minute):
                             difference = int(closing_minute) - minute
-                            await client.say(f"{content} is OPEN now! {hours_of_operation} It will be closing in {difference} minutes!")
+                            await closing_soon
                         else:
                             await closed
+                    # if hour is not the same as closing hour but there is still 1hr or less until closing
                     elif (closing - hour) == 1:
                         if int(closing_minute) == 0:
                             difference = 60 - minute
                         else:
                             difference = int(closing_minute) - minute
-                        await client.say(f"{content} is OPEN now! {hours_of_operation} It will be closing in {difference} minutes!")
+                        await closing_soon
                     else:
                         await open
+                # if hour is not the same as opening hour but there is still 1hr or less until opening
+                elif (opening - hour) == 1:
+                    if int(opening_minute) == 0:
+                        difference = 60 - minute
+                    else:
+                        difference = int(opening_minute) - minute
+                    await opening_soon
                 else:
                     await closed
-            await client.say('https://nudining.com/hours')
+        # generates link to NUDining's hours of operation
+        await client.say('https://nudining.com/hours')
+    # if invalid location was provided
     else:
-        await client.say("Error: Location options are: Stwest, Steast, IV, Outtakes, Rebecca's, UBurger, Kigo's Kitchen, Starbucks, Subway, Popeyes, Qdoba.")
+        await client.say(f"Error: Location options are: {POSSIBLE_LOCATIONS}")
 
 
+# gives the ice-cream flavors on the menu for today
 @client.command()
 async def icecream(*args):
     POSSIBLE_DAYS = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY']
     EST = datetime.now(timezone('US/Eastern'))
+    # if day is given
     if args:
+        # if given day is tomorrow
         if args[0].upper() == 'TOMORROW':
+            # finds the current day's index in POSSIBLE_DAYS and add 1 getting the next day
             for index in range(0, len(POSSIBLE_DAYS)):
                 if EST.strftime("%A").upper() == POSSIBLE_DAYS[index]:
                     day = POSSIBLE_DAYS[(index+1) % len(POSSIBLE_DAYS)]
                     break
+        # if valid day
         elif args[0].upper() in POSSIBLE_DAYS:
             day = args[0].upper()
+        # if not a valid day
         else:
             await client.say("Error: Not a valid day.")
+    # if no day is given
     else:
+        # set day to current day
         day = EST.strftime("%A").upper()
     flavors = NUDining.ICE_CREAM_FLAVORS[day]
     await client.say(f"There is {flavors} on {day}.")
 
 
-@client.command()  # coin flip
+# coin flip
+@client.command()
 async def flip():
     outcome = random.randint(0, 1)
     if outcome == 0:
@@ -520,7 +610,8 @@ async def flip():
         await client.say("Tails!")
 
 
-@client.command()  # stops bot
+# stops bot
+@client.command()
 @commands.has_role('Admin')
 async def logout():
     await client.say("Alright I'll stop now.")
@@ -528,12 +619,13 @@ async def logout():
     await client.logout()
 
 
+# loads all extensions
 if __name__ == '__main__':
     for extension in EXTENSIONS:
         try:
             client.load_extension(extension)
         except Exception as error:
-            print('{} cannot be loaded. [{}]'.format(extension, error))
+            print(f"{extension} cannot be loaded. [{error}]")
 client.loop.create_task(change_status())  # iniate loop for status
 # client.loop.create_task(not_registered_reminder())  # reminder for not-registered channel
 client.run(TOKEN)  # run bot
