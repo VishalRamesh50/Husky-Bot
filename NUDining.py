9cc8fb39c33d3e46e55d35b1f9e5b05bd2a95e3f
Vishal Ramesh <vishalramesh50@gmail.com>
2019-02-01 04:10:00 -0500

    add amelia's, shwarma, cappys, chicken lous, cc, cvs, dominos, dictionary of location aliases, unique links

--- NUDining.py
+++ NUDining.py
@@ -4,56 +4,106 @@ IV = {'MONDAY': [7, '00', 'AM', 22, '00', 'PM'],
       'WEDNESDAY': [7, '00', 'AM', 22, '00', 'PM'],
       'THURSDAY': [7, '00', 'AM', 22, '00', 'PM'],
       'FRIDAY': [7, '00', 'AM', 21, '00', 'PM'],
-      'WEEKENDS': [8, '00', 'AM', 21, '00', 'PM']}
+      'WEEKENDS': [8, '00', 'AM', 21, '00', 'PM'],
+      'LINK': 'https://nudining.com/hours'}
 STEAST = {'MONDAY': [7, '00', 'AM', 23, '00', 'PM'],
           'TUESDAY': [7, '00', 'AM', 23, '00', 'PM'],
           'WEDNESDAY': [7, '00', 'AM', 23, '00', 'PM'],
           'THURSDAY': [7, '00', 'AM', 23, '00', 'PM'],
           'FRIDAY': [7, '00', 'AM', 22, '00', 'PM'],
-          'WEEKENDS': [8, '00', 'AM', 22, '00', 'PM']}
+          'WEEKENDS': [8, '00', 'AM', 22, '00', 'PM'],
+          'LINK': 'https://nudining.com/hours'}
 STWEST = {'MONDAY': [11, '00', 'AM', 20, '00', 'PM'],
           'TUESDAY': [11, '00', 'AM', 20, '00', 'PM'],
           'WEDNESDAY': [11, '00', 'AM', 20, '00', 'PM'],
           'THURSDAY': [11, '00', 'AM', 20, '00', 'PM'],
           'FRIDAY': [11, '00', 'AM', 17, '00', 'PM'],
           'SATURDAY': "CLOSED",
-          'SUNDAY': [16, '00', 'PM', 20, '00', 'PM']}
+          'SUNDAY': [16, '00', 'PM', 20, '00', 'PM'],
+          'LINK': 'https://nudining.com/hours'}
 OUTTAKES = {'MONDAY': [11, '00', 'AM', 13, '00', 'PM'],
             'TUESDAY': [11, '00', 'AM', 13, '00', 'PM'],
             'WEDNESDAY': [11, '00', 'AM', 13, '00', 'PM'],
             'THURSDAY': [11, '00', 'AM', 25, '00', 'AM'],
             'FRIDAY': [11, '00', 'AM', 20, '00', 'PM'],
             'SATURDAY': [12, '00', 'PM', 18, '00', 'PM'],
-            'SUNDAY': "CLOSED"}
+            'SUNDAY': "CLOSED",
+            'LINK': 'https://nudining.com/hours'}
 KIGO = {'MONDAY': [11, '00', 'AM', 19, '00', 'PM'],
         'TUESDAY': [11, '00', 'AM', 19, '00', 'PM'],
         'WEDNESDAY': [11, '00', 'AM', 19, '00', 'PM'],
         'THURSDAY': [11, '00', 'AM', 19, '00', 'PM'],
         'FRIDAY': [11, '00', 'AM', 16, '00', 'PM'],
-        'WEEKENDS': "CLOSED"}
+        'WEEKENDS': "CLOSED",
+        'LINK': 'https://nudining.com/hours'}
 POPEYES = {'WEEKDAYS': [10, '30', 'AM', 21, '00', 'PM'],
            'SATURDAY': [10, '30', 'AM', 20, '00', 'PM'],
-           'SUNDAY': [12, '00', 'PM',  19, '00', 'PM']}
+           'SUNDAY': [12, '00', 'PM',  19, '00', 'PM'],
+           'LINK': 'https://nudining.com/hours'}
 REBECCAS = {'WEEKDAYS': [7, '00', 'AM', 16, '00', 'PM'],
-            'WEEKENDS': "CLOSED"}
+            'WEEKENDS': "CLOSED",
+            'LINK': 'https://nudining.com/hours'}
 STARBUCKS = {'WEEKDAYS': [7, '00', 'AM', 21, '00', 'PM'],
              'SATURDAY': [8, '00', 'AM', 21, '00', 'PM'],
-             'SUNDAY': [10, '00', 'AM',  6, '00', 'PM']}
+             'SUNDAY': [10, '00', 'AM',  6, '00', 'PM'],
+             'LINK': 'https://nudining.com/hours'}
 SUBWAY = {'MONDAY': [8, '00', 'AM', 16, '30', 'PM'],
           'TUESDAY': [8, '00', 'AM', 16, '30', 'PM'],
           'WEDNESDAY': [8, '00', 'AM', 16, '30', 'PM'],
           'THURSDAY': [8, '00', 'AM', 16, '30', 'PM'],
           'FRIDAY': [11, '00', 'AM', 16, '00', 'PM'],
-          'WEEKENDS': "CLOSED"}
+          'WEEKENDS': "CLOSED",
+          'LINK': 'https://nudining.com/hours'}
 UBURGER = {'WEEKDAYS': [11, '00', 'AM', 20, '00', 'PM'],
            'SATURDAY': [11, '00', 'AM', 19, '00', 'PM'],
-           'SUNDAY': [12, '00', 'PM', 19, '00', 'PM']}
+           'SUNDAY': [12, '00', 'PM', 19, '00', 'PM'],
+           'LINK': 'https://nudining.com/hours'}
 QDOBA = {'MONDAY': [10, '30', 'AM', 23, '00', 'PM'],
          'TUESDAY': [10, '30', 'AM', 23, '00', 'PM'],
          'WEDNESDAY': [10, '30', 'AM', 23, '00', 'PM'],
          'THURSDAY': [10, '30', 'AM', 24, '00', 'AM'],
          'FRIDAY': [10, '30', 'AM', 24, '00', 'AM'],
-         'WEEKEENDS': [10, '30', 'AM', 20, '00', 'PM']}
+         'WEEKEENDS': [10, '30', 'AM', 20, '00', 'PM'],
+         'LINK': 'https://locations.qdoba.com/us/ma/boston/393-huntington-ave.html'}
+AMELIAS = {'MONDAY': [7, '00', 'AM', 23, '00', 'PM'],
+           'TUESDAY': [7, '00', 'AM', 23, '00', 'PM'],
+           'WEDNESDAY': [7, '00', 'AM', 23, '00', 'PM'],
+           'THURSDAY': [7, '00', 'AM', 23, '00', 'PM'],
+           'FRIDAY': [7, '00', 'AM', 14, '00', 'PM'],
+           'SATURDAY': [7, '00', 'AM', 14, '00', 'PM'],
+           'SUNDAY': [7, '30', 'AM', 22, '00', 'PM'],
+           'LINK': 'https://ameliastaqueria.com/locations/huntington-ave/'}
+BOSTON_SHAWARMA = {'EVERYDAY': [10, '00', 'AM', 23, '30', 'PM'],
+                   'LINK': 'http://www.bostonshawarma.net'}
+CAPPYS = {'EVERYDAY': [6, '30', 'AM', 26, '30', 'PM'],
+          'LINK': 'http://cappyspizza.com/indexs.php'}
+CHICKEN_LOUS = {'MONDAY': [6, '45', 'AM', 16, '30', 'PM'],
+                'TUESDAY': [6, '45', 'AM', 16, '30', 'PM'],
+                'WEDNESDAY': [6, '45', 'AM', 16, '30', 'PM'],
+                'THURSDAY': [6, '45', 'AM', 16, '30', 'PM'],
+                'FRIDAY': [6, '45', 'AM', 18, '45', 'PM'],
+                'SATURDAY': [7, '30', 'AM', 14, '30', 'PM'],
+                'SUNDAY': "CLOSED",
+                'LINK': 'https://www.chickenlous.com'}
+COLLEGE_CONVENIENCE = {'EVERYDAY': [0, '00', 'AM', 24, '00', 'PM'],
+                       'LINK': 'https://menupages.com/college-convenience/281-huntington-ave-boston/#about'}
+CVS = {"EVERYDAY": [7, '00', 'AM', 24, '00', 'PM'],
+       'LINK': 'https://www.cvs.com/store-locator/cvs-pharmacy-address/231+Massachusetts+Avenue-Boston-MA-02115/storeid=1259'}
+DOMINOS = {"EVERYDAY": [10, '30', 'AM', 27, '00', 'AM'],
+           'LINK': 'https://pizza.dominos.com/massachusetts/boston/1260-boylston-st/'}
+
+#  Dictionary of locations & aliases for normal hours
+NORMAL_LOCATIONS = {('INTERNATIONAL VILLAGE', 'IV'): IV, ('STETSON EAST', 'STEAST'): STEAST,
+                    ('STETSON WEST', 'STWEST'): STWEST, ('OUTTAKES', 'OUTTAKES'): OUTTAKES,
+                    ('KIGO KITCHEN', 'KIGO'): KIGO, ('POPEYES LOUSIANA KITCHEN', 'POPEYES KITCHEN', 'POPEYES'): POPEYES,
+                    ("REBECCA'S", 'REBECCAS'): REBECCAS, ('STARBUCKS', 'STARBUCKS'): STARBUCKS,
+                    ('SUBWAY', 'SUBWAY'): SUBWAY, ('UBURGER', 'UBURGER'): UBURGER,
+                    ('QDOBA', 'QDOBA'): QDOBA, ("AMELIA'S", 'AMELIAS'): AMELIAS,
+                    ('BOSTON SHAWARMA', 'SHAWARMA'): BOSTON_SHAWARMA, ("CAPPY'S", 'CAPPYS'): CAPPYS,
+                    ("CHICKEN LOU'S", "CHICKEN LOUS", 'CHICKEN_LOUS', 'CL'): CHICKEN_LOUS,
+                    ('COLLEGE CONVENIENCE', 'COLLEGE_CONVENIENCE', 'CC'): COLLEGE_CONVENIENCE,
+                    ('CVS', 'CVS'): CVS, ('DOMINOS', 'DOMINOS'): DOMINOS}
+
 
 # ice-cream flavors
 ICE_CREAM_FLAVORS = {'MONDAY': 'STRAWBERRY & CHOCOLATE 🍨',
@@ -106,3 +156,11 @@ UBURGER_MARTIN = {'FRIDAY': [11, '00', 'AM', 20, '00', 'PM'],
                   'SUNDAY': 'CLOSED',
                   'MONDAY': [11, '30', 'AM', 19, '00', 'PM']}
 QDOBA_MARTIN = QDOBA
+
+#  Dictionary of locations & aliases for MLK hours
+MLK_LOCATIONS = {('INTERNATIONAL VILLAGE', 'IV'): IV_MARTIN, ('STETSON EAST', 'STEAST'): STEAST_MARTIN,
+                 ('STETSON WEST', 'STWEST'): STWEST_MARTIN, ('OUTTAKES', 'OUTTAKES'): OUTTAKES_MARTIN,
+                 ('KIGO KITCHEN', 'KIGO'): KIGO_MARTIN, ('POPEYES LOUSIANA KITCHEN', 'POPEYES KITCHEN', 'POPEYES'): POPEYES_MARTIN,
+                 ("REBECCA'S", 'REBECCAS'): REBECCAS_MARTIN, ('STARBUCKS', 'STARBUCKS'): STARBUCKS_MARTIN,
+                 ('SUBWAY', 'SUBWAY'): SUBWAY_MARTIN, ('UBURGER', 'UBURGER'): UBURGER_MARTIN,
+                 ('QDOBA', 'QDOBA'): QDOBA_MARTIN}
