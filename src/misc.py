36a0d22dd7a6820b7368b60278b21799220c7229
Vishal Ramesh <vishalramesh50@gmail.com>
2019-05-10 20:28:58 -0400

    mention Admin

--- src/misc.py
+++ src/misc.py
@@ -91,7 +91,7 @@ class Misc(commands.Cog):
         SUGGESTIONS_CHANNEL = self.client.get_channel(SUGGESTIONS_CHANNEL_ID)
         ADMIN_ROLE = discord.utils.get(guild.roles, name='Admin')
 
-        await SUGGESTIONS_CHANNEL.send(f"**{ADMIN_ROLE} {author.mention} suggests:**\n"
+        await SUGGESTIONS_CHANNEL.send(f"**{ADMIN_ROLE.mention} {author.mention} suggests:**\n"
                                        f"*{suggestion}*")
         await SUGGESTIONS_CHANNEL.last_message.pin()
 
