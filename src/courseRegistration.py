9aaf3002b65b52be033688933721b40290dd484d
Vishal Ramesh <vishalramesh50@gmail.com>
2019-09-13 00:57:23 -0400

    notifies admins when course didn't exist using choose command + comments

--- src/courseRegistration.py
+++ src/courseRegistration.py
@@ -1,8 +1,10 @@
 import discord
 from discord.ext import commands
 import asyncio
+import re
 
 COURSE_REGISTRATION_CHANNEL_ID = 485279507582943262
+ADMIN_CHANNEL_ID = 485269858557100034
 
 
 class CourseRegistration(commands.Cog):
@@ -53,36 +55,52 @@ class CourseRegistration(commands.Cog):
         message = ctx.message
         guild = ctx.guild
         member = message.author
+        ADMIN_CHANNEL = self.client.get_channel(ADMIN_CHANNEL_ID)
         admin = member.permissions_in(ctx.channel).administrator
-        await message.delete()
         args = ' '.join(args).strip()
         role = None
+        await message.delete()  # deletes command
+        # tries to find a role given the user input
         try:
             role = await commands.RoleConverter().convert(ctx, args)
+        # if a role could not be found
         except discord.ext.commands.errors.BadArgument as e:
             for r in guild.roles:
+                # if the given input is the same as a role when lowercase or/and ignoring dashes
                 if (args.lower() == r.name.lower() or
                    self.ignoreDashCase(args) == self.ignoreDashCase(r.name)):
                     role = r
                     break
+            # if no role exists
             if role is None:
-                await ctx.send(e, delete_after=5)
+                await ctx.send(e, delete_after=5)  # sends an error message to user about missing role
+                pattern = re.compile(r'^[A-Z]{2}([A-Z]{2})?-\d{2}[\dA-Z]{2}$')  # regex pattern for courses
+                # if the given argument is the format of a course with dashes subbed in for spaces or normally
+                if pattern.match(args.upper()) or pattern.match(args.replace(' ', '-').upper()):
+                    # notify the admins and tell the user about this notification.
+                    await ADMIN_CHANNEL.send(f"{member.mention} just tried to add `{args}` using the `.choose` command. Consider adding this course.")
+                    await ctx.send(f"The course `{args}` is not available but I have notified the admin team to add it.", delete_after=5)
                 return
+        # roles that a normal user is allowed to add other than course roles
         WHITELISTED_COLLEGES = ['CCIS', 'COE', 'BCHS', 'CAMD', 'DMSB', 'COS', 'CSSH', 'EXPLORE']
-        WHITELISTED_COLORS = ['ORANGE', 'LIGHT GREEN', 'YELLOW', 'PURPLE', 'LIGHT BLUE', 'PINK', 'LIGHT PINK', 'PALE PINK', 'CYAN', 'SPRING GREEN', 'PALE YELLOW', 'NAVY BLUE']
+        WHITELISTED_COLORS = ['ORANGE', 'LIGHT GREEN', 'YELLOW', 'PURPLE', 'LIGHT BLUE', 'PINK', 'LIGHT PINK', 'PALE PINK', 'CYAN', 'SPRING GREEN', 'PALE YELLOW', 'NAVY BLUE', 'LAVENDER']
+        # if the role is one of the whitelisted roles or the user is an admin
         if ('-' in role.name or role.name in WHITELISTED_COLLEGES + WHITELISTED_COLORS or admin):
-            if (role in member.roles):
-                try:
+            # try to add or a remove a role to/from a user
+            try:
+                # if the role is already one of the user's role
+                if (role in member.roles):
+                    # remove the role from the user
                     await member.remove_roles(role)
-                    await ctx.send(f"{role.name} has been removed", delete_after=5)
-                except discord.errors.Forbidden:
-                    await ctx.send("I do not have permission to alter that role", delete_after=5)
-            else:
-                try:
+                    await ctx.send(f"`{role.name}` has been removed.", delete_after=5)
+                else:
+                    # add the role to the user
                     await member.add_roles(role)
-                    await ctx.send(f"{role.name} has been added!", delete_after=5)
-                except discord.errors.Forbidden:
-                    await ctx.send("I do not have permission to alter that role", delete_after=5)
+                    await ctx.send(f"`{role.name}` has been added!", delete_after=5)
+            # in case of insufficient bot permissions
+            except discord.errors.Forbidden:
+                await ctx.send("I do not have permission to alter that role", delete_after=5)
+        # if the role is not one of the whitelisted courses members are allowed to add.
         else:
             await ctx.send("You do not have the permission to toggle that role 🙃", delete_after=5)
 
