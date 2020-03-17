e9077e1b8a720454f593f843cde21303b7d9db1f
Vishal Ramesh <vishalramesh50@gmail.com>
2020-03-16 20:01:53 -0400

    Move error handler to separate cog

--- src/main.py
+++ src/main.py
@@ -2,18 +2,9 @@ import asyncio
 import discord
 import logging
 import os
-import sys
-import traceback
-from datetime import datetime
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 from itertools import cycle
-
-from data.ids import (
-    BOT_SPAM_CHANNEL_ID,
-    COURSE_REGISTRATION_CHANNEL_ID,
-    ERROR_LOG_CHANNEL_ID,
-)
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
@@ -27,6 +18,7 @@ EXTENSIONS = [
     "clear",
     "course_registration",
     "day",
+    "error_handler",
     "help",
     "hours.hours",
     "icecream",
@@ -64,123 +56,6 @@ async def change_status() -> None:
         current_status = next(msgs)
         await client.change_presence(activity=discord.Game(current_status))
         await asyncio.sleep(5)
-
-
-@client.event
-async def on_error(event_method: str, *args, **kwargs) -> None:
-    """Global error handler to catch all errors thrown.
-
-    Will send an embedded message in the #error-log channel.
-    Includes traceback, Exception name, args, guild, and timestamp.
-
-    Parameters
-    ------------
-    event_method: `str`
-        The method which the exception was thrown in.
-    *args:
-        The arguments the method was called with.
-    **kwargs:
-        The keyword arguments the method was called with.
-    """
-
-    logger.error(f"Some error with {event_method}!")
-    ERROR_LOG_CHANNEL: discord.TextChannel = client.get_channel(ERROR_LOG_CHANNEL_ID)
-    err_type, error, tb = sys.exc_info()
-
-    if error is None:
-        # custom error added when coming from on_command_error
-        error = kwargs.get("thrown_error")
-        if error is None:
-            return
-
-    og_cause = error.__cause__
-    if og_cause:
-        error = og_cause
-
-    tb = error.__traceback__
-    extracted_tb = traceback.extract_tb(tb)
-    tb_content = "".join(extracted_tb.format())
-
-    # Notify of exception
-    embed = discord.Embed(
-        title=f"{error.__class__.__name__} {str(error)}",
-        timestamp=datetime.utcnow(),
-        description=f"```{tb_content}```" if tb_content else "",
-        colour=discord.Colour.red(),
-    )
-
-    embed.set_author(name=f"Command/Event: {event_method}")
-    embed.set_footer(text=kwargs.get("thrown_guild", "Unknown Guild"))
-
-    for arg in args:
-        arg_name = str(arg) or '""'
-        if arg_name.startswith("<") and "." in arg_name:
-            arg_type = arg.__class__.__name__
-        else:
-            arg_type = type(arg)
-        embed.add_field(name=arg_type, value=arg_name, inline=True)
-
-        # if there is the guild property on this arg
-        if "guild" in dir(arg):
-            embed.set_footer(text=f"Guild: {arg.guild}")
-
-    await ERROR_LOG_CHANNEL.send(embed=embed)
-    traceback.print_exception(err_type, error, tb)
-
-
-# error handler
-@client.event
-async def on_command_error(ctx: commands.Context, error: commands.CommandError) -> None:
-    """Global command_error handler to catch all errors thrown.
-
-    Some logic to ignore certain exceptions and gracefully redirect is in here.
-    If no recognized error can be ignored it will be sent to the on_error and
-    set a keyword `thrown_error` and `guild` on the kwargs called with on_error.
-
-    Parameters
-    ------------
-    ctx: `commands.Context`
-        A class containing metadata about the command invocation.
-    error: `commands.CommandError`
-        The exception which triggered the calling of this method.
-    """
-
-    # try to get the original error if one exists
-    og_cause = error.__cause__
-    if og_cause:
-        error = og_cause
-
-    if isinstance(error, commands.CheckFailure):
-        # ----------------------- COURSE-REGSISTRATION CHECK ----------------------
-        COURSE_REGISTRATION_CHANNEL = client.get_channel(COURSE_REGISTRATION_CHANNEL_ID)
-        if str(error) == "The check functions for command choose failed.":
-            await ctx.message.delete()
-            await ctx.send(
-                f"Not here! Try again in {COURSE_REGISTRATION_CHANNEL.mention}",
-                delete_after=5,
-            )
-            return
-        # -------------------------------------------------------------------------
-
-        # --------------------------- BOT-SPAM CHECK ------------------------------
-        BOT_SPAM_CHANNEL = client.get_channel(BOT_SPAM_CHANNEL_ID)
-        bot_spam_necessary_commands = ["hours", "open", "ping", "whois"]
-        for command in bot_spam_necessary_commands:
-            if str(error) == f"The check functions for command {command} failed.":
-                await ctx.message.delete()
-                await ctx.send(
-                    f"Not here! Try again in {BOT_SPAM_CHANNEL.mention}", delete_after=5
-                )
-                return
-        # -------------------------------------------------------------------------
-    elif isinstance(error, commands.CommandNotFound):
-        # if the prefix is in the error it probably wasn't meant to be a command
-        if PREFIX in str(error):
-            return
-    # add the error onto the kwargs for the on_error to have access
-    ctx.kwargs["thrown_error"] = error
-    ctx.kwargs["thrown_guild"] = ctx.guild
-    await on_error(ctx.command, *ctx.args, **ctx.kwargs)
 
 
 # disable DM commands
