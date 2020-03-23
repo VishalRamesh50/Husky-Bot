d37eb6fdfbbbca52861ed139325c8f27705c2614
Vishal Ramesh <vishalramesh50@gmail.com>
2020-03-23 00:08:30 -0400

    Update misc cog
    - Add docstrings.
    - Send escaped message content for echo.
    - Delete invite command.
    - Add some checks for commands.

--- src/cogs/misc.py
+++ src/cogs/misc.py
@@ -1,3 +1,4 @@
+import discord
 import random
 from discord.ext import commands
 from checks import is_admin, in_channel, is_mod
@@ -9,42 +10,60 @@ class Misc(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    # replies Pong! given .ping
     @commands.command()
     @commands.check_any(in_channel(BOT_SPAM_CHANNEL_ID), is_admin(), is_mod())
-    async def ping(self, ctx):
+    async def ping(self, ctx: commands.Context):
+        """Sends a message which contains the Discord WebSocket protocol latency.
+
+        Parameters
+        -------------
+        ctx: `commands.Context`
+            A class containing metadata about the command invocation.
+        """
+
         await ctx.send(f"Pong! {round(self.client.latency * 1000)}ms")
 
-    # repeats given statement after .echo
+    @is_admin()
     @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def echo(self, ctx, *args):
-        message = ctx.message
-        output = ""
-        for word in args:
-            output += word
-            output += " "
-        await message.delete()  # deletes command
-        await ctx.send(output)
+    async def echo(self, ctx: commands.Context) -> None:
+        """Repeats the message given after the command.
+        Will use the escaped content which does not mention anything.
 
-    # coin flip
+        Parameters
+        -------------
+        ctx: `commands.Context`
+            A class containing metadata about the command invocation.
+        """
+        message: discord.Message = ctx.message
+        await message.delete()
+        await ctx.send(message.clean_content[6:])
+
     @commands.command()
-    async def flip(self, ctx):
+    @commands.check_any(in_channel(BOT_SPAM_CHANNEL_ID), is_admin(), is_mod())
+    async def flip(self, ctx: commands.Context) -> None:
+        """Flips an imaginary coin and sends the result.
+
+        Parameters
+        -------------
+        ctx: `commands.Context`
+            A class containing metadata about the command invocation.
+        """
+
+        results = ["Heads!", "Tails!"]
         outcome = random.randint(0, 1)
-        if outcome == 0:
-            await ctx.send("Heads!")
-        else:
-            await ctx.send("Tails!")
+        await ctx.send(results[outcome])
 
-    # generates link to NU dining menu
     @commands.command()
-    async def menu(self, ctx):
+    @commands.check_any(in_channel(BOT_SPAM_CHANNEL_ID), is_admin(), is_mod())
+    async def menu(self, ctx: commands.Context) -> None:
+        """Sends a link to the NU dining menu.
+
+        Parameters
+        -------------
+        ctx: `commands.Context`
+            A class containing metadata about the command invocation.
+        """
         await ctx.send("https://nudining.com/public/menus")
-
-    # generates invite link to server
-    @commands.command()
-    async def invite(self, ctx):
-        await ctx.send("discord.gg/8HHcup8")
 
 
 def setup(client):
