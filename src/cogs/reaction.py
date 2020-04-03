57e921cac5030b55d2f978b0e189d7e3cee45b34
Vishal Ramesh <vishalramesh50@gmail.com>
2020-04-02 22:55:07 -0400

    Quick fix to make old course-registration code compatible with new reaction code

--- src/cogs/reaction.py
+++ src/cogs/reaction.py
@@ -444,7 +444,7 @@ class Reaction(commands.Cog):
                     # edit the course description message with the new course inserted
                     await descriptionMessage.edit(content=content)
                     # create a reaction role for the embeded message
-                    await ctx.invoke(self.newrr, *[COURSE_REGISTRATION_CHANNEL.mention, message.id, emoji, courseRole.mention])
+                    await ctx.invoke(self.newrr, *[COURSE_REGISTRATION_CHANNEL, message.id, emoji, courseRole])
                     return
         await ctx.send(f"No embedded message was found for the category `{courseCategory}`")
 
