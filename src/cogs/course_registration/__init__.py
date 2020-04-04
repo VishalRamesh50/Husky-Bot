f3dd263c13aed1bb31e3153b1098ae6bc7608564
Vishal Ramesh <vishalramesh50@gmail.com>
2020-04-04 19:18:02 -0400

    Load all cogs in module by re-exporting and setting up
    - This allows the initial extensions list to contain just the module name, but each cog can still be individually loaded and unloaded if the full path is specified

---
+++ src/cogs/course_registration/__init__.py
@@ -0,0 +1,11 @@
+from discord.ext import commands
+
+from .course_registration import CourseRegistration
+from .course_selection import CourseSelection
+from .create_course import CreateCourse
+
+
+def setup(client: commands.Bot):
+    client.add_cog(CourseRegistration(client))
+    client.add_cog(CourseSelection(client))
+    client.add_cog(CreateCourse(client))
