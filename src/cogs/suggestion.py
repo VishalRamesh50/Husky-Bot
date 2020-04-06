761d2e7938a98b040764e3ac6caddbfedea373ab
Vishal Ramesh <vishalramesh50@gmail.com>
2020-04-05 22:12:29 -0400

    Add docstrings to suggestions command
    - Also get channel from guild and remove unecssary assignment to author.

--- src/cogs/suggestion.py
+++ src/cogs/suggestion.py
@@ -5,22 +5,34 @@ from data.ids import SUGGESTIONS_CHANNEL_ID
 
 
 class Suggestion(commands.Cog):
-    def __init__(self, client):
+    """Controls the ability to make visible suggestions."""
+
+    def __init__(self, client: commands.Bot):
         self.client = client
 
-    # allows users to create suggestions
+    @commands.guild_only()
     @commands.command()
-    async def suggest(self, ctx, *, suggestion):
-        author = ctx.author
-        guild = ctx.guild
-        SUGGESTIONS_CHANNEL = self.client.get_channel(SUGGESTIONS_CHANNEL_ID)
+    async def suggest(self, ctx: commands.Context, *, suggestion: str) -> None:
+        """Will post a formatted message of the suggestion made by a user in
+        the suggestions channel, pings the Admins and pins the message.
+
+        Parameters
+        -----------
+        ctx: `commands.Context`
+            A class containing metadata about the command invocation.
+        suggestion: `str`
+            The suggestion the the user made.
+        """
+        guild: discord.Guild = ctx.guild
+        SUGGESTIONS_CHANNEL = guild.get_channel(SUGGESTIONS_CHANNEL_ID)
         ADMIN_ROLE = discord.utils.get(guild.roles, name="Admin")
 
         await SUGGESTIONS_CHANNEL.send(
-            f"**{ADMIN_ROLE.mention} {author.mention} suggests:**\n" f"*{suggestion}*"
+            f"**{ADMIN_ROLE.mention} {ctx.author.mention} suggests:**\n"
+            f"*{suggestion}*"
         )
         await SUGGESTIONS_CHANNEL.last_message.pin()
 
 
-def setup(client):
+def setup(client: commands.Bot):
     client.add_cog(Suggestion(client))
