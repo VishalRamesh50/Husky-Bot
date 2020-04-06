9e09e06e5774dece318bddf8b1871c7392e98db1
Vishal Ramesh <45186928+VishalRamesh50@users.noreply.github.com>
2020-04-05 20:44:26 -0400

    Rewrite Stats (#30)
    * Use more efficient collection.Counter instead of iterating through members manually
    
    - I initially thought that looping over the members and increasing counters would be more efficient that using collections.Counter multiple times. However, on average collections.Counter performs faster.
    - In the grand scheme of things the difference isn't that great, but it makes sense to use in this case.
    
    * Rewrite orderedListMembers command
    
    - Get rid of unecessary check inside loop
    - Get rid of unecessary assignment
    
    * Let joinNo command call the whois command
    
    - This prevents the duplication of the logic involved in getting/displaying the member's info.
    
    * Refactor whois command
    
    - Create a new FuzzyMemberConverter which checks all the things from a normal converter but also check case-insensitive and even if the argument is just in the name vs an exact match.
    - Update private format method to take UTC time and convert to a US/Eastern time formatted string instead of using UTC time.
    - Include timezone in formatted string.
    - Also don't do a check if result is None to raise exception in converters.
         - Instead return right away when it finds a match. Then if the end of the function is reached this means nothing was found and an exception can be raised.

--- src/cogs/stats.py
+++ src/cogs/stats.py
@@ -1,19 +1,22 @@
 import discord
+from collections import Counter
 from datetime import datetime
 from discord.ext import commands
 from pytz import timezone
+from typing import Optional
 
-from checks import is_admin, in_channel, is_mod
-from data.ids import BOT_SPAM_CHANNEL_ID, NOT_REGISTERED_ROLE_ID
+from checks import in_channel, is_admin, is_mod
+from converters import FuzzyMemberConverter
+from data.ids import BOT_SPAM_CHANNEL_ID
 
 
 class Stats(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
 
-    # displays some server info
     @commands.command()
-    @commands.has_role('Moderator')
+    @commands.guild_only()
+    @commands.check_any(is_admin(), is_mod())
     async def serverinfo(self, ctx: commands.Context) -> None:
         """
         Sends an embedded message containing some stats about the server.
@@ -30,22 +33,26 @@ class Stats(commands.Cog):
         Note: A New Account is considered to be an account which was created within 1 day of joining the server.
         """
         guild: discord.Guild = ctx.guild
-        numBots = newAccounts = online = idle = dnd = online_mobile = idle_mobile = dnd_mobile = not_registered_count = 0
-        NOT_REGISTERED: discord.Role = guild.get_role(NOT_REGISTERED_ROLE_ID)
-        for member in guild.members:
-            numBots += 1 if member.bot else 0
-            join_diff = (member.joined_at - member.created_at).days
-            newAccounts += 1 if (join_diff <= 1) else 0
-            online += 1 if (member.status == discord.Status.online) else 0
-            idle += 1 if (member.status == discord.Status.idle) else 0
-            dnd += 1 if (member.status == discord.Status.dnd) else 0
-            online_mobile += 1 if (member.status == discord.Status.online and member.is_on_mobile()) else 0
-            idle_mobile += 1 if (member.status == discord.Status.idle and member.is_on_mobile()) else 0
-            dnd_mobile += 1 if (member.status == discord.Status.dnd and member.is_on_mobile()) else 0
-            not_registered_count += 1 if (NOT_REGISTERED in member.roles) else 0
+        NOT_REGISTERED: discord.Role = discord.utils.get(
+            guild.roles, name="Not Registered"
+        )
+        new_accounts: int = Counter(
+            [(m.joined_at - m.created_at).days <= 1 for m in guild.members]
+        )[True]
+        not_registered_count: int = Counter(
+            [NOT_REGISTERED in m.roles for m in guild.members]
+        )[True]
+        num_bots: int = Counter([m.bot for m in guild.members])[True]
+        statuses = Counter([(m.status, m.is_on_mobile()) for m in guild.members])
+        online_mobile: int = statuses[(discord.Status.online, True)]
+        idle_mobile: int = statuses[(discord.Status.idle, True)]
+        dnd_mobile: int = statuses[(discord.Status.dnd, True)]
+        online: int = statuses[(discord.Status.online, False)] + online_mobile
+        idle: int = statuses[(discord.Status.idle, False)] + idle_mobile
+        dnd: int = statuses[(discord.Status.dnd, False)] + dnd_mobile
+        return
 
         embed = discord.Embed(colour=discord.Colour.red(), timestamp=guild.created_at)
-
         embed.set_author(name=guild, icon_url=guild.icon_url)
         embed.set_footer(text=f"Server ID: {guild.id} | Server Created")
 
@@ -56,24 +63,28 @@ class Stats(commands.Cog):
         embed.add_field(name="Voice Channels", value=len(guild.voice_channels))
         embed.add_field(name="Roles", value=len(guild.roles))
         embed.add_field(name="Members", value=guild.member_count)
-        embed.add_field(name="Humans", value=guild.member_count - numBots)
-        embed.add_field(name="Bots", value=numBots)
+        embed.add_field(name="Humans", value=guild.member_count - num_bots)
+        embed.add_field(name="Bots", value=num_bots)
         embed.add_field(name="Online", value=f"{online} | Mobile: {online_mobile}")
         embed.add_field(name="Idle", value=f"{idle} | Mobile: {idle_mobile}")
         embed.add_field(name="Dnd", value=f"{dnd} | Mobile: {dnd_mobile}")
-        embed.add_field(name='Not Registered', value=not_registered_count)
-        embed.add_field(name="New Accounts", value=newAccounts)
-        embed.add_field(name="Emojis", value=f'{len(guild.emojis)}/{guild.emoji_limit}')
+        embed.add_field(name="Not Registered", value=not_registered_count)
+        embed.add_field(name="New Accounts", value=new_accounts)
+        embed.add_field(name="Emojis", value=f"{len(guild.emojis)}/{guild.emoji_limit}")
         embed.add_field(name="Verification Level", value=guild.verification_level)
         embed.add_field(name="Active Invites", value=len(await guild.invites()))
         embed.add_field(name="2FA", value=bool(guild.mfa_level))
 
         await ctx.send(embed=embed)
 
-    # displays a list of a given number of members ordered by join date
-    @commands.command()
-    @commands.has_role('Moderator')
-    async def orderedListMembers(self, ctx: commands.Context, num: int = 10, outputType: str = "nickname") -> None:
+    @commands.command(
+        aliases=["orderedListMembers", "lsMembers", "listMembers", "list_members"]
+    )
+    @commands.guild_only()
+    @commands.check_any(is_admin(), is_mod())
+    async def ordered_list_members(
+        self, ctx: commands.Context, num: int = 10, outputType: str = "nickname"
+    ) -> None:
         """
         Sends an embedded message containing a list of members in order by
         when the joined the server.
@@ -91,114 +102,62 @@ class Stats(commands.Cog):
             Name will give a list of usernames
             Mention will give a list of mentioned users.
         """
-        try:
-            num = int(num)
-        except ValueError:
-            await ctx.send("Provide a valid integer.")
-            return
-        EST = datetime.now(timezone('US/Eastern'))  # EST timezone
-        allMembers = ctx.guild.members
-        sortedMembers = sorted(allMembers, key=lambda m: m.joined_at)
-        msg = ""
-        count = 0
-        embed = discord.Embed(colour=discord.Color.red(), timestamp=EST)
-        for member in sortedMembers:
-            if (count < num):
-                if (outputType == 'nickname' or outputType == 'nick'):
+        msg: str = ""
+        count: int = 0
+        embed = discord.Embed(colour=discord.Color.red(), timestamp=datetime.utcnow())
+        for member in sorted(ctx.guild.members, key=lambda m: m.joined_at):
+            if count < num:
+                if outputType == "nickname" or outputType == "nick":
                     msg += member.display_name + ", "
-                elif (outputType == 'name'):
+                elif outputType == "name":
                     msg += member.name + ", "
-                elif (outputType == 'mention'):
+                elif outputType == "mention":
                     msg += member.mention + ", "
                 else:
-                    await ctx.send("Valid display type not given. Try: nickname/nick/name/mention")
+                    await ctx.send(
+                        "Valid display type not given. Try: nickname/nick/name/mention"
+                    )
                     return
                 count += 1
-                # if there are at least 10 members
-                if (len(sortedMembers) >= 10):
-                    # if 10 members were reached add to the message
-                    if (count % 10 == 0):
-                        embed.add_field(name=f"__{count - 9}-{count}:__", value=msg[:len(msg) - 2])
-                        msg = ""
-                    # if 100 members were reached send the embed and reset it to start a new one
-                    if (count % 100 == 0):
-                        await ctx.send(embed=embed)
-                        embed = discord.Embed(colour=discord.Color.red(), timestamp=EST)
+                if count % 10 == 0:
+                    embed.add_field(name=f"__{count - 9}-{count}:__", value=msg[:-2])
+                    msg = ""
+                if count % 100 == 0:
+                    await ctx.send(embed=embed)
+                    embed = discord.Embed(
+                        colour=discord.Color.red(), timestamp=datetime.utcnow()
+                    )
         # if an even 10 people was not reached
-        if (msg != ""):
-            embed.add_field(name=f"__{count - count % 10 + 1}-{count}:__", value=msg[:len(msg) - 2])
+        if msg != "":
+            embed.add_field(
+                name=f"__{count - (count % 10) + 1}-{count}:__", value=msg[:-2]
+            )
             await ctx.send(embed=embed)
-        # if less than 100 members given
-        elif (msg == ""):
+        # if less than 100 members was reached
+        elif msg == "":
             await ctx.send(embed=embed)
 
-    # format a date with Day, Month Date, Year Hour: Minute: Seconds removing 0 padding
-    def formatDate(self, input):
-        return input.strftime("%a, %b %d, %Y %I:%M:%S %p").replace(" 0", " ")
-
-    # displays some information about a user who joined the server at the join position
-    @commands.command()
-    @commands.has_role('Moderator')
-    async def joinNo(self, ctx: commands.Context, num: int) -> None:
-        """
-        Sends an embedded message containing information about the user at the
-        given joinNo.
+    def __formatted_date(self, date: datetime) -> str:
+        """Takes what is assumed to be a utc timezone, converts it to US/Eastern time
+        and then formats it as a string.
 
         Parameters
-        -----------
-        ctx: `commands.Context`
-            A class containing metadata about the command invocation.
-        num: `int`
-            The joinNo of the user to get information about.
+        ------------
+        date: `datetime`
+            A timezone naive datetime which is assumed to be UTC.
+
+        Returns
+        ---------
+        A string in the format: Day, Mon Date, YYYY H:MM:SS AM/PM (Timezone)
         """
-        guild = ctx.guild
-        allMembers = guild.members
-        EST = datetime.now(timezone('US/Eastern'))  # EST timezone
-        try:
-            num = int(num)
-        except ValueError:
-            await ctx.send("Enter a valid integer.")
-            return
-        if (num <= 0):
-            await ctx.send("Number must be a positive non-zero number.")
-            return
-        sortedMembers = sorted(allMembers, key=lambda m: m.joined_at)
-        if num <= len(sortedMembers):
-            member = sortedMembers[num - 1]
-        else:
-            await ctx.send(f"{guild} only has {guild.member_count} members!")
-            return
-        roles = ''
-        for role in member.roles[1:]:
-            roles += role.mention + ' '
-        # display NO ROLES if user has no roles
-        roles = 'NO ROLES' if roles == '' else roles
-        permissions = ''
-        for perm in member.guild_permissions:
-            name = perm[0].replace('_', ' ').title()
-            value = perm[1]
-            if value:
-                permissions += (name + ', ')
-        permissions = permissions[:-2]
-        embed = discord.Embed(colour=member.color, timestamp=EST, description=member.mention)
+        utc_date: datetime = timezone("UTC").localize(date)
+        est_date: datetime = utc_date.astimezone(timezone("US/Eastern"))
+        return est_date.strftime("%a, %b %d, %Y %-I:%M:%S %p (%Z)")
 
-        embed.set_thumbnail(url=member.avatar_url)
-        embed.set_author(name=member, icon_url=member.avatar_url)
-        embed.set_footer(text=f"Member ID: {member.id}")
-
-        embed.add_field(name="Status", value=member.status)
-        embed.add_field(name="Joined", value=self.formatDate(member.joined_at))
-        embed.add_field(name="Join Position", value=num)
-        embed.add_field(name="Created At", value=self.formatDate(member.created_at))
-        embed.add_field(name=f"Roles ({len(member.roles) - 1})", value=roles)
-        embed.add_field(name="Key Permissions", value=permissions)
-
-        await ctx.send(embed=embed)
-
-    # displays some information about a given member
-    @commands.command(aliases=['whoam'])
+    @commands.command(aliases=["whoam"])
+    @commands.guild_only()
     @commands.check_any(in_channel(BOT_SPAM_CHANNEL_ID), is_admin(), is_mod())
-    async def whois(self, ctx: commands.Context, *args) -> None:
+    async def whois(self, ctx: commands.Context, *member_name_parts) -> None:
         """
         Sends an embedded message containing information about the given user.
 
@@ -206,58 +165,53 @@ class Stats(commands.Cog):
         -----------
         ctx: `commands.Context`
             A class containing metadata about the command invocation.
-        *args:
-            The arguments to the command, ideally a member name.
+        member_parts `Tuple`:
+            The member's name as a tuple of strings.
         """
-        member = None
-        args = ' '.join(args)
-        # if no argument was given or I was given set member to the user calling the command
-        if args == '' or args.upper() == 'I':
+        member: Optional[discord.Member] = None
+        member_name: str = " ".join(member_name_parts)
+        if member_name == "" or member_name.upper() == "I":
             member = ctx.author
         else:
-            # try to convert the argument into a valid member
             try:
-                member = await commands.MemberConverter().convert(ctx, args)
-            # if unable to convert argument to a member
+                member = await FuzzyMemberConverter().convert(ctx, member_name)
             except discord.ext.commands.errors.BadArgument as e:
-                for m in ctx.guild.members:
-                    # if the given arguments are part of a valid member's name or nickname
-                    if args.lower() in m.display_name.lower() or args.lower() in m.name.lower():
-                        member = m
-                        break
-                # if member has still not been assigned send error message
-                if member is None:
-                    await ctx.send(e)
-                    return
-        EST = datetime.now(timezone('US/Eastern'))  # EST timezone
-        # if user has an administrator permissions
-        admin = ctx.author.permissions_in(ctx.channel).administrator
-        mod = discord.utils.get(ctx.author.roles, name='Moderator')
-        if ctx.author == member or admin or mod:
-            allMembers = ctx.guild.members
-            sortedMembers = sorted(allMembers, key=lambda m: m.joined_at)
-            joinPosition = sortedMembers.index(member) + 1
-            roles = ''
-            for role in member.roles[1:]:
-                roles += role.mention + ' '
-            # display NO ROLES if user has no roles
-            roles = 'NO ROLES' if roles == '' else roles
-            permissions = ''
-            for perm in member.guild_permissions:
-                name = perm[0].replace('_', ' ').title()
-                value = perm[1]
-                if value:
-                    permissions += (name + ', ')
-            permissions = permissions[:-2]
-            embed = discord.Embed(colour=member.color, timestamp=EST, description=member.mention)
+                await ctx.send(e)
+                return
 
+        admin: bool = ctx.author.permissions_in(ctx.channel).administrator
+        mod: bool = discord.utils.get(ctx.author.roles, name="Moderator")
+        if ctx.author == member or admin or mod:
+            join_position: int = sorted(
+                ctx.guild.members, key=lambda m: m.joined_at
+            ).index(member) + 1
+            roles: str = ""
+            for role in member.roles[1:]:
+                roles += role.mention + " "
+            roles = "NO ROLES" if roles == "" else roles
+            permissions: str = ""
+            for perm in member.guild_permissions:
+                if perm[1]:
+                    perm_name = perm[0].replace("_", " ").title()
+                    permissions += perm_name + ", "
+            permissions = permissions[:-2]
+
+            embed = discord.Embed(
+                colour=member.color,
+                timestamp=datetime.utcnow(),
+                description=member.mention,
+            )
             embed.set_thumbnail(url=member.avatar_url)
             embed.set_author(name=member, icon_url=member.avatar_url)
             embed.set_footer(text=f"Member ID: {member.id}")
             embed.add_field(name="Status", value=member.status)
-            embed.add_field(name="Joined", value=self.formatDate(member.joined_at))
-            embed.add_field(name="Join Position", value=joinPosition)
-            embed.add_field(name="Created At", value=self.formatDate(member.created_at))
+            embed.add_field(
+                name="Joined", value=self.__formatted_date(member.joined_at)
+            )
+            embed.add_field(name="Join Position", value=join_position)
+            embed.add_field(
+                name="Created At", value=self.__formatted_date(member.created_at)
+            )
             embed.add_field(name=f"Roles ({len(member.roles) - 1})", value=roles)
             embed.add_field(name="Key Permissions", value=permissions)
 
@@ -265,6 +219,34 @@ class Stats(commands.Cog):
         else:
             await ctx.message.delete()
             await ctx.send("Stop snooping around where you shouldn't 🙃", delete_after=5)
+
+    @commands.command(aliases=["joinNo", "joinPosition", "joinPos"])
+    @commands.guild_only()
+    @commands.check_any(is_admin(), is_mod())
+    async def join_no(self, ctx: commands.Context, join_no: int) -> None:
+        """
+        Sends an embedded message containing information about the user who joined
+        the server at the given join position.
+
+        Parameters
+        -----------
+        ctx: `commands.Context`
+            A class containing metadata about the command invocation.
+        join_no: `int`
+            The join position of the user to get information about.
+        """
+        guild: discord.Guild = ctx.guild
+        if join_no <= 0:
+            await ctx.send("Number must be a positive non-zero number.")
+            return
+        try:
+            member: discord.Member = sorted(guild.members, key=lambda m: m.joined_at)[
+                join_no - 1
+            ]
+        except IndexError:
+            await ctx.send(f"{guild} only has {guild.member_count} members!")
+            return
+        await self.whois(ctx, member.name)
 
 
 def setup(client):
